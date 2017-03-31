a8720df2de045a1937a1f0541d350b7fa0ce5181
Steven Bustos <jsbustosb@unal.edu.co>
2017-03-31 05:51:16 -0500

    Mejoras en el frontend
    Se mejoraron algunas vistas, se editaron algunos css

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,7 +10,6 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( nav_gentelella.css )
-Rails.application.config.assets.precompile += %w( grayscale.css )
 Rails.application.config.assets.precompile += %w( style.css )
 Rails.application.config.assets.precompile += %w( css/menu_vertical.css )
 Rails.application.config.assets.precompile += %w( css/menu_horizontal.css )

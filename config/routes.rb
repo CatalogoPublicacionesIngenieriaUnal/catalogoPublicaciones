f98650759fa2228eaf31edf51f771849abceae13
David <dsbarrerag@unal.edu.co>
2017-04-06 15:23:09 -0500

    Correccion de rutas

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,19 @@
 Rails.application.routes.draw do
-  resources :keywords do 
-  
-  end
-  resources :attachments do
 
-    root to: "home#index"
+
+  root to: "home#index"
+
   devise_scope :professor do
     get "/logout" => "devise/sessions#destroy", :as => :destroy_user_session
-
   end
     devise_for :judges
     devise_for :administrators
     devise_for :professors, path: '', path_names: { sign_in: '', sign_out: 'logout'}
 
+  resources :keywords do
 
+  end
+  resources :attachments do
   end
   resources :professor_publications do
   end
@@ -22,7 +22,7 @@ Rails.application.routes.draw do
   end
   resources :approved_publications do
 
-  end  
+  end
   resources :publications do
 
   end
@@ -38,9 +38,9 @@ Rails.application.routes.draw do
   resources :evaluations, except: :delete do
     match 'evaluacion', to: 'evaluations#edit', via: [:put,:post]
     match 'edit', to: 'evaluations#show', via: [:get]
-    resources :application_requests, except: :destroy 
+    resources :application_requests, except: :destroy
   end
-  resources :judges do 
+  resources :judges do
   end
   resources :professors, except: :delete do
     match 'edit', to: 'professors#show', via: [:get]
@@ -52,7 +52,7 @@ Rails.application.routes.draw do
     end
     resources :publications, only: :get
   end
-  resources :administrators, except: :delete do 
+  resources :administrators, except: :delete do
     resources :judges, except: :delete do
       match 'new', to: 'judges#show', via: [:get]
       match 'edit', to: 'judges#show', via: [:get]
@@ -60,7 +60,7 @@ Rails.application.routes.draw do
     resources :application_requests, except: :delete do
       match 'edit', to: 'application_requests#show', via: [:get]
       match 'asignar_evaluadores', to: 'application_requests#show', via: [:get]
-    end 
+    end
     resources :publications, except: :delete do
       match 'new', to: 'publications#show', via: [:get]
       match 'edit', to: 'publications#show', via: [:get]

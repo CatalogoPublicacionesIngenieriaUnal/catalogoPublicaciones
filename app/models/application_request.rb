ea40f61c0d5cfd6d41c5e789b22f0fc2b172dce6
Ivan Gonzalez <ivdgonzalezco@unal.edu.co>
2017-04-11 00:40:59 -0500

    Queries

--- app/models/application_request.rb
+++ app/models/application_request.rb
@@ -4,4 +4,15 @@ class ApplicationRequest < ApplicationRecord
   has_many :professors, through: :professor_application_requests
   has_many :evaluations
   
+	def self.load_applications_by_state_id(id,page = 1, per_page = 10)
+      includes(:state).where("state_id = ?", id)
+  end
+
+  def self.load_applications_by_professor_id(id,page = 1, per_page = 10)
+      includes(:professor_application_requests,:professors).where("id = ?",id)
+  end
+
+  def self.find_by_evaluation_status(id)
+    includes(:evaluations).where('state_id = ?', id)
+  end
 end

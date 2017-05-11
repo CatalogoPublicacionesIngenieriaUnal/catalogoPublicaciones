515a0a6005e8defd2d09a0d1031dbbc20ae1e403
Steven Bustos <jsbustosb@unal.edu.co>
2017-05-11 16:22:22 -0500

    Validaciones por front y back

--- app/models/ed_con_app_request.rb
+++ app/models/ed_con_app_request.rb
@@ -1,4 +1,7 @@
 class EdConAppRequest < ApplicationRecord
   belongs_to :editorial_concept_criterium
   belongs_to :application_request
+
+  validates :score, presence: true
+  validates :remark, presence: true
 end
